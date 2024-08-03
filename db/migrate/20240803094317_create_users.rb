86fa5dee29e3d9d3e5181c15b3d4585ac87f50ce
James Lagattuta <jimmy.lagattuta@gmail.com>
2024-08-03 02:44:24 -0700

    initial commit

--- db/migrate/20240803094317_create_users.rb
+++ db/migrate/20240803094317_create_users.rb
@@ -1,8 +1,9 @@
 class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
-      t.string :email
-      t.string :password_digest
+      t.string :username
+      t.string :password
+      t.string :password_confirmation
 
       t.timestamps
     end
