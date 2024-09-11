ff7eba33c5ff45289b03f049697de651b28a6f68
James Lagattuta <jimmy.lagattuta@gmail.com>
2024-09-10 22:36:00 -0700

    updated pictures

--- client/src/pages/main/homeSections/Intro.js
+++ client/src/pages/main/homeSections/Intro.js
@@ -5,7 +5,7 @@ const images = [
   'https://i.imgur.com/HDAlZ7d.jpeg',
   'https://i.imgur.com/fiqweb3.jpeg',
   'https://i.imgur.com/dRbhXia.jpeg',
-  'https://i.imgur.com/Y0EB8Q4.jpeg',
+  'https://i.imgur.com/7K3Uz6A.jpeg', 
   'https://i.imgur.com/azdpeAP.jpeg'
 ];
 
@@ -14,8 +14,9 @@ const Intro = () => {
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setCurrentImage((prevImage) => (prevImage + 1) % images.length);
-    }, 3000); // Switches images every 3 seconds
+      setCurrentImage((prevImage) => (prevImage + 1) % images.length); // Change image every 3s
+    }, 3000);
+
     return () => clearInterval(interval);
   }, []);
 
@@ -25,15 +26,18 @@ const Intro = () => {
         <div className="hero-text">
           <h1>Your Slogan Goes Here</h1>
           <p>Here is a short description of whatever you want.</p>
-          <button className="hero-button">Lorem Ipsum</button>
+          <button className="hero-button">Any Button</button>
         </div>
       </div>
       <div className="hero-image-container">
-        <img
-          src={images[currentImage]}
-          alt="Golf Cart"
-          className="hero-image"
-        />
+        {images.map((image, index) => (
+          <img
+            key={index}
+            src={image}
+            alt="Slideshow"
+            className={`hero-image ${index === currentImage ? 'fade-in' : 'fade-out'}`}
+          />
+        ))}
       </div>
     </div>
   );
