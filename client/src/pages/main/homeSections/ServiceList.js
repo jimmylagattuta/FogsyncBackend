65f2f30331280798a2f2871f2e1137dac74f86a3
James Lagattuta <jimmy.lagattuta@gmail.com>
2024-09-12 07:45:30 -0700

    added in ease effect to service section

--- client/src/pages/main/homeSections/ServiceList.js
+++ client/src/pages/main/homeSections/ServiceList.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import './ServiceList.css';
 
 const services = [
@@ -13,14 +13,43 @@ const services = [
 ];
 
 function ServiceList() {
+  const serviceRefs = useRef([]);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            entry.target.classList.add('ease-in');
+          }
+        });
+      },
+      { threshold: 0.1 } // Trigger when 10% of the item is visible
+    );
+
+    serviceRefs.current.forEach((ref) => {
+      if (ref) observer.observe(ref);
+    });
+
+    return () => {
+      serviceRefs.current.forEach((ref) => {
+        if (ref) observer.unobserve(ref);
+      });
+    };
+  }, []);
+
   return (
     <section className="service-list-section">
       <h1 className="service-list-title">Explore Our Services</h1>
-      <i className="fab fa-opencart title-symbol"></i> {/* No inline styles */}
-      
+      <i className="fab fa-opencart title-symbol"></i>
+
       <div className="service-list-grid">
         {services.map((service, index) => (
-          <div className="service-item" key={index}>
+          <div
+            className="service-item"
+            key={index}
+            ref={(el) => (serviceRefs.current[index] = el)}
+          >
             <img src={service.image} alt={service.name} className="service-image" />
             <h3 className="service-name">{service.name}</h3>
           </div>
