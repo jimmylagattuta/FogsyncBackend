865af764c1372fa37f2899208f4512cfa8c62618
James Lagattuta <jimmy.lagattuta@gmail.com>
2024-10-23 15:03:53 -0700

    changing responsive map design

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,7 @@
 require_relative "boot"
 
 require "rails/all"
+require_relative "../lib/middleware/force_www"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -8,6 +9,8 @@ Bundler.require(*Rails.groups)
 
 module BcbCarts
   class Application < Rails::Application
+    config.middleware.use ForceWWW
+
     config.load_defaults 6.1
     config.api_only = true
 
