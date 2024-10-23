3681d0ccfaa7f6d981470baeb6e9a222a03f0eab
James Lagattuta <jimmy.lagattuta@gmail.com>
2024-10-23 16:24:53 -0700

    changing responsive map design

--- lib/middleware/force_www.rb
+++ lib/middleware/force_www.rb
@@ -7,11 +7,17 @@ class ForceWWW
     def call(env)
       request = Rack::Request.new(env)
   
+      # Debugging statements
+      puts "Host: #{request.host}"
+      puts "Full URL: #{request.url}"
+  
       # Check if the host does not start with 'www'
       if request.host !~ /^www\./
+        puts "Redirecting to: #{request.url.sub('//', '//www.')}"
         # Redirect to the same path but with 'www'
         [301, { 'Location' => request.url.sub("//", "//www.") }, []]
       else
+        puts "Request already contains 'www'."
         @app.call(env)
       end
     end
