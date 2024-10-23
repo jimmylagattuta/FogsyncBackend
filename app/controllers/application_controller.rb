bc6ef3e007d1b87868c20cb10ca53aa632d2562d
James Lagattuta <jimmy.lagattuta@gmail.com>
2024-10-23 15:23:56 -0700

    changing responsive map design

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::API
     include ActionController::Cookies
     before_action :authenticate_user
+    before_action :ensure_www_subdomain
 
     def current_user
         User.find_by(id: session[:user_id])
@@ -8,4 +9,12 @@ class ApplicationController < ActionController::API
     def authenticate_user
         return render json: { error: "Not authorized" }, status: :unauthorized unless current_user
     end
+
+    private
+    def ensure_www_subdomain
+        if request.host == "laorthos.com"
+          redirect_to "#{request.protocol}www.laorthos.com#{request.fullpath}", status: :moved_permanently
+        end
+    end
+    
 end
