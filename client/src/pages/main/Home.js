ca4d5b86b87108d9620df07892fe20ae8ead2dd7
James Lagattuta <jimmy.lagattuta@gmail.com>
2024-11-03 21:26:07 -0800

    resetting

--- client/src/pages/main/Home.js
+++ client/src/pages/main/Home.js
@@ -6,7 +6,7 @@ import Company from './homeSections/Company';
 import AboutSection from './homeSections/AboutSection';
 import ServiceList from './homeSections/ServiceList';
 import MapContainer from './homeSections/MapContainer';
-import ReviewsComponent from './homeSections/ReviewsComponent';
+import ReviewsComponent from '../../components/ReviewsComponent';
 
 const Home = ({ reviews }) => {
   console.log('Home');
