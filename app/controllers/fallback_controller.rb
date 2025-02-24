f5828f1a94e79f2df599d120f14dc7cf9662b701
James Lagattuta <jimmy.lagattuta@gmail.com>
2025-02-23 19:27:44 -0800

    initial commit

--- app/controllers/fallback_controller.rb
+++ app/controllers/fallback_controller.rb
@@ -1,8 +1,5 @@
 class FallbackController < ActionController::Base
-	def index
-	    response.headers['Cache-Control'] = 'public, max-age=3600, immutable'
-	    response.headers['Surrogate-Control'] = 'max-age=3600'
-		render file: 'public/index.html'
-		# render file: 'es20client/public/index.html'
-	end
-end
+    def index
+      render file: 'public/index.html'
+    end
+  end
