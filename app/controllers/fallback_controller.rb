d12a57ed3ce5c7813ac4c6ffe2dd4de3d4f7d5d4
James Lagattuta <jimmy.lagattuta@gmail.com>
2025-02-23 19:25:06 -0800

    initial commit

--- app/controllers/fallback_controller.rb
+++ app/controllers/fallback_controller.rb
@@ -1,15 +1,8 @@
 class FallbackController < ActionController::Base
-  # Handle HTML GET requests for unmatched routes
-  def index
-    render file: 'public/index.html', layout: false
-  end
-
-  # Handle all other types of unmatched requests
-  def all
-    respond_to do |format|
-      format.json { render json: { error: "Not Found" }, status: :not_found }
-      format.html { render file: 'public/404.html', status: :not_found, layout: false }
-      format.any { head :not_found }
-    end
-  end
+	def index
+	    response.headers['Cache-Control'] = 'public, max-age=3600, immutable'
+	    response.headers['Surrogate-Control'] = 'max-age=3600'
+		render file: 'public/index.html'
+		# render file: 'es20client/public/index.html'
+	end
 end
