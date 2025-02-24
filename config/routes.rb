f342dfcd8d4b995e5070f0afad5d49155477fee7
James Lagattuta <jimmy.lagattuta@gmail.com>
2025-02-23 19:44:46 -0800

    initial commit

--- config/routes.rb
+++ config/routes.rb
@@ -5,13 +5,13 @@ Rails.application.routes.draw do
   # root "articles#index"
   root "fallback#index"
 
-  post '/signup', to: "api/users#create"
-  get '/me', to: "api/users#show"
-  post '/login', to: "api/sessions#create"
-  delete "/logout", to: "api/sessions#destroy"  
-  delete "/logout", to: "api/sessions#destroy"
-  post '/contact', to: 'api/contacts#create'
-  get "/pull_yelp_cache", to: "api/jobs#pull_yelp_cache"
+  # post '/signup', to: "api/users#create"
+  # get '/me', to: "api/users#show"
+  # post '/login', to: "api/sessions#create"
+  # delete "/logout", to: "api/sessions#destroy"  
+  # delete "/logout", to: "api/sessions#destroy"
+  # post '/contact', to: 'api/contacts#create'
+  # get "/pull_yelp_cache", to: "api/jobs#pull_yelp_cache"
 
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 
